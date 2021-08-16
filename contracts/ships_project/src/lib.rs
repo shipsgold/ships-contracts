07580a5f8b4364ac19105f7740e9855fcb23f096
Zane Starr <zcstarr@gmail.com>
2021-08-16 15:12:05 -0700

    feat: add support for releases wip

--- contracts/ships_project/src/lib.rs
+++ contracts/ships_project/src/lib.rs
@@ -1,8 +1,9 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::{ValidAccountId, U128, Base58PublicKey};
+use near_sdk::json_types::{ValidAccountId, U128, Base58PublicKey, U64};
 use near_sdk::serde::{Serialize, Deserialize};
 use near_sdk::{BorshStorageKey, ext_contract, Balance, PublicKey, env, near_bindgen, setup_alloc, AccountId, PanicOnDefault, Promise, PromiseResult, StorageUsage};
 use near_sdk::log;
+use std::cmp::Ordering;
 use multi_token_standard::metadata::{MultiTokenMetadata};
 
 
@@ -16,6 +17,8 @@ mod account;
 mod page;
 
 use page::{PaginationOptions,PaginationResponse};
+use crate::ReleaseStatus::ACTIVE;
+
 type CodeId = String;
 setup_alloc!();
 
@@ -52,6 +55,29 @@ pub struct Project {
   id: ProjectId, 
   details: ProjectDetails
 }
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, PartialEq)]
+#[cfg_attr(test, derive(Clone, Debug))]
+pub struct Version {
+    minor: u32,
+    major: u32,
+    patch: u32
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let major = self.major.cmp(&other.major);
+        if major != Ordering::Equal {
+           return Some(major)
+        }
+        let minor = self.minor.cmp(&other.minor);
+        if minor != Ordering::Equal {
+            return Some(minor)
+        }
+        return Some(self.patch.cmp(&other.patch))
+    }
+}
+
 // This allows for the project details to change
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[cfg_attr(test, derive(Clone, Debug))]
@@ -63,14 +89,36 @@ pub struct ProjectDetails {
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[cfg_attr(test, derive(Clone, Debug))]
+pub struct ReleaseDetails {
+    name: String,
+    version: Version,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[cfg_attr(test, derive(Clone, Debug))]
+pub struct ReleaseTerms {
+    min: u32,
+    max: u32,
+    pre_allocation: U128
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, PartialEq)]
+#[cfg_attr(test, derive(Clone, Debug))]
+pub enum ReleaseStatus {
+    ACTIVE,
+    CLOSED
+}
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[cfg_attr(test, derive(Clone, Debug))]
 pub struct Release {
-  version: String,
+  version: Version,
   releaser: AccountId,
   name: String,
   min: u32,
   max: u32,
   release_id: ReleaseId, 
-  pre_allocation: Balance
+  pre_allocation: Balance,
+  status: ReleaseStatus
 }
 
 #[near_bindgen]
@@ -83,8 +131,10 @@ pub struct Contract {
     project_hash_to_project_id: LookupMap<ProjectHash, ProjectId>,
     project_id_to_project: LookupMap<ProjectId, Project>,
     project_to_releases: LookupMap<ProjectId, Vector<Release>>,
-    project_storage_cost: Balance,
-    user_storage_cost: Balance,
+    project_storage_usage: u64, 
+    user_storage_usage: u64,
+    guest_storage_usage: u64,
+    release_storage_usage: u64,
     project_id_idx: u64,
     release_id_idx: u64,
     prefix_project_to_release_idx: u64,
@@ -108,15 +158,17 @@ impl Contract {
             project_hash_to_project_id: LookupMap::new(StorageKey::ProjectHashToProjectId),
             project_id_to_project: LookupMap::new(StorageKey::ProjectIdsToProjects),
             project_to_releases: LookupMap::new(StorageKey::ProjectToReleases),
-            project_storage_cost: 0,
-            user_storage_cost: 0,
+            project_storage_usage: 0,
+            user_storage_usage: 0,
+            guest_storage_usage: 0,
+            release_storage_usage: 0,
             release_id_idx: 0,
             project_id_idx: 0,
             prefix_owner_to_projects_idx: 0,
             prefix_project_to_release_idx: 0
         };
-        this.project_storage_cost = this.min_project_storage_cost();
-        this.user_storage_cost = this.min_user_storage_cost();
+        this.measure_project_storage_usage();
+        this.measure_user_storage_usage();
         this
     }
 
@@ -140,7 +192,7 @@ impl Contract {
         self.prefix_project_to_release_idx
     }
 
-    fn min_user_storage_cost(&mut self) -> u128 {
+    fn measure_user_storage_usage(&mut self) {
         let initial_storage_usage = env::storage_usage();
         let tmp_owner_id = unsafe { String::from_utf8_unchecked(vec![b'a'; 64]) };
         let tmp_projects = Vector::new(
@@ -154,25 +206,27 @@ impl Contract {
         // cleanup
         self.owner_to_projects.remove(&tmp_owner_id);
 
-        u128::from(storage_usage - initial_storage_usage) * env::storage_byte_cost()
+        self.user_storage_usage = (storage_usage - initial_storage_usage);
     }
 
-    fn min_guest_storage_cost(&mut self) -> u128 {
+    fn min_guest_storage_cost(&mut self) {
         let initial_storage_usage = env::storage_usage();
         let tmp_guest_id = vec![b'a'; 64];
         self.guests.insert(&tmp_guest_id);
         let storage_usage = env::storage_usage();
         self.guests.remove(&tmp_guest_id);
-        let storage_used:u128 = (storage_usage - initial_storage_usage).into();
-        let cost = storage_used * env::storage_byte_cost();
-        cost.checked_add(self.min_user_storage_cost()).unwrap()
+        if self.user_storage_usage == 0 {
+           self.measure_user_storage_usage();
+        }
+        self.guest_storage_usage = (storage_usage - initial_storage_usage).checked_add(self.user_storage_usage).unwrap();
     }
 
     fn calculate_project_hash(&self, project_details: &ProjectDetails) ->Vec<u8> {
         env::sha256(&project_details.try_to_vec().unwrap())      
     }
 
-    fn min_project_storage_cost(&mut self) -> u128{
+
+    fn measure_project_storage_usage(&mut self){
         let tmp_owner_id = unsafe { String::from_utf8_unchecked(vec![b'a'; 64]) };
         let tmp_details = unsafe { String::from_utf8_unchecked(vec![b'a'; 64]) };
         let tmp_uri= unsafe { String::from_utf8_unchecked(vec![b'u'; 250]) };
@@ -180,6 +234,10 @@ impl Contract {
            StorageKey::ProjectIdToProject{project_id: 
             self.prefix_owner_to_projects_idx.to_be_bytes().to_vec()
         });
+        let tmp_releases = Vector::new(
+            StorageKey::ProjectReleases{project_id:
+            self.prefix_project_to_release_idx.to_be_bytes().to_vec()
+            });
 
 
         let project_details = ProjectDetails {
@@ -191,7 +249,7 @@ impl Contract {
         let project =  Project {
             id: self.project_id_idx,
             name: tmp_owner_id.to_string(),
-            owner: tmp_owner_id,
+            owner: tmp_owner_id.clone(),
             uri: tmp_uri.clone(), 
             details: project_details
         };
@@ -201,31 +259,55 @@ impl Contract {
         self.project_id_to_project.insert(&project.id, &project);
         let project_hash = self.calculate_project_hash(&project.details);
         self.project_hash_to_project_id.insert(&project_hash, &project.id);
-        let storage_usage = env::storage_usage();
+        self.project_to_releases.insert(&project.id,&tmp_releases);
+        let project_storage_usage = env::storage_usage();
         // clean up
+        self.project_storage_usage = project_storage_usage - initial_storage_usage;
+
+        let mut releases = self.project_to_releases.get(&project.id).unwrap();
+        releases.push(&Release {
+            releaser: tmp_owner_id.clone(),
+            release_id: self.inc_release_idx(),
+            pre_allocation: 9u128,
+            min: 10000,
+            max: 20000,
+            version: Version { major: 0, minor: 1, patch: 1},
+            name: tmp_details.clone(),
+            status: ReleaseStatus::ACTIVE
+        });
+        self.project_to_releases.insert(&project.id, &releases);
+        self.release_storage_usage = (env::storage_usage() - project_storage_usage);
+
+        self.project_to_releases.remove(&project.id);
         self.owner_to_projects.remove(&project.owner);
         self.project_id_to_project.remove(&project.id);
         self.project_hash_to_project_id.remove(&project_hash);
-        u128::from(storage_usage - initial_storage_usage) * env::storage_byte_cost()
     }
 
     #[payable]
     pub fn register_user(&mut self) {
-        let refund = env::attached_deposit().checked_sub(self.user_storage_cost)
-        .unwrap_or_else(|| panic!("Project requires at least {} deposit",self.user_storage_cost));   
+        let storage_cost = u128::from(self.user_storage_usage) * env::storage_byte_cost();
+        let refund = env::attached_deposit().checked_sub(storage_cost)
+        .unwrap_or_else(|| panic!("Project requires at least {} deposit",storage_cost));
         let owner_id = env::predecessor_account_id();
         let tmp_projects = Vector::new(
-           StorageKey::ProjectIdToProject{project_id: 
-            self.prefix_owner_to_projects_idx.to_be_bytes().to_vec()
+           StorageKey::ProjectIdToProject{project_id:
+            self.inc_prefix_owner_to_projects().to_be_bytes().to_vec()
         });
+        ;
         self.owner_to_projects.insert(&owner_id, &tmp_projects);
     }
 
     #[payable]
     pub fn create_project(&mut self, name: String, uri: String, details: ProjectDetails) {
         let owner_id = env::predecessor_account_id();
-        let refund = env::attached_deposit().checked_sub(self.project_storage_cost)
-        .unwrap_or_else(|| panic!("Project requires at least {} deposit",self.project_storage_cost));   
+        let project_storage_cost = env::storage_byte_cost() * u128::from(self.project_storage_usage);
+        let refund = env::attached_deposit().checked_sub(project_storage_cost)
+        .unwrap_or_else(|| panic!("Project requires at least {} deposit", project_storage_cost));
+        let releases = Vector::new(
+            StorageKey::ProjectReleases{project_id:
+             self.inc_prefix_project_to_release().to_be_bytes().to_vec()
+            });
 
         let project_hash = self.calculate_project_hash(&details);
         let project_id = self.inc_project_idx();
@@ -243,12 +325,12 @@ impl Contract {
         projects.push(&project_id);
         self.owner_to_projects.insert(&project.owner, &projects);
         self.project_id_to_project.insert(&project.id, &project);
+        self.project_to_releases.insert(&project.id, &releases);
 
         if refund > 0 {
             Promise::new(env::predecessor_account_id()).transfer(refund);
         }
         log!("project created {}", project.name);
-        
     }
 
     pub fn get_project(&self, project_id: ProjectId) -> Option<Project>{
@@ -270,9 +352,59 @@ impl Contract {
     }
 
     #[payable]
-    pub fn create_new_release(&mut self, project_id: String,  version: String){
+    pub fn create_new_release(&mut self, project_id: ProjectId,  details: ReleaseDetails, terms: ReleaseTerms){
+        let project = self.project_id_to_project.get(&project_id).unwrap_or_else(|| panic!("Project id: {} does not exist", project_id));
+        assert_eq!(project.owner, env::predecessor_account_id());
 
+        // calculate refund
+        let release_storage_cost = env::storage_byte_cost() * u128::from(self.release_storage_usage);
+        let refund = env::attached_deposit().checked_sub(release_storage_cost)
+            .unwrap_or_else(|| panic!("Project requires at least {} deposit", release_storage_cost));
 
+        // only callable by owner of the project
+        let mut releases = self.project_to_releases.get(&project_id).unwrap();
+        //verify version is increasing in nature and verify there are no active releases
+        if releases.len() > 0 {
+            let release = releases.get(releases.len() - 1).unwrap();
+            assert_eq!(release.status, ReleaseStatus::CLOSED);
+            if release.version > details.version {
+                panic!("Version is less than latest version {} {} {}",
+                       release.version.major,
+                       release.version.minor,
+                       release.version.patch);
+            }
+        }
+
+        releases.push(&Release {
+            name: details.name,
+            version: details.version,
+            max: terms.max,
+            min: terms.min,
+            pre_allocation: terms.pre_allocation.into(),
+            release_id: releases.len(),
+            releaser: env::predecessor_account_id(),
+            status: ACTIVE
+        });
+        self.project_to_releases.insert(&project_id, &releases);
+    }
+
+    pub fn get_release(&self, project_id: ProjectId, release_id: ReleaseId)-> Option<Release> {
+        let releases = self.project_to_releases.get(&project_id).unwrap();
+        releases.get(release_id)
+    }
+
+    pub fn get_releases(&self, project_id: ProjectId, options: Option<PaginationOptions>)->Vec<Release> {
+        let releases = self.project_to_releases.get(&project_id).unwrap();
+        let opt = options.unwrap_or_default();
+        let mut range = (opt.from..std::cmp::min(opt.from + opt.limit, releases.len()));
+
+        if opt.reverse {
+            let from = std::cmp::min(opt.from - opt.limit, 0);
+            range = (from..std::cmp::min(opt.from, releases.len()));
+        }
+        range
+            .map(|index| releases.get(index).unwrap())
+            .collect()
     }
 
     #[payable]
@@ -360,7 +492,24 @@ mod tests {
         });
         let project = contract.get_project(1);
         let projects = contract.get_projects("bob".to_string(), None);
-        println!("{:?}", project);
+        contract.create_new_release(1, ReleaseDetails{
+            version: Version {
+                major: 1,
+                minor: 0,
+                patch: 0
+            },
+            name: "EdgyEgret".to_string()
+        },
+        ReleaseTerms{
+            min: 10000,
+            max: 20000,
+            pre_allocation: 100.into()
+        });
+        let release = contract.get_release(1,0).unwrap();
+        println!("{:?}", release);
+       let releases = contract.get_releases(1, None);
+        println!("{:?}", releases);
+
 /*        let mut contract = Contract {
             owner: accounts(1),
             val: 0,
